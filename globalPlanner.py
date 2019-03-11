2157cf73694e7ffd5ae25b97be35d4a61d35d5db
KevinNordenhog <kevin.nordenhog@gmail.com>
2019-03-11 15:58:19 +0100

    implemented moving agents and multiple agents

--- globalPlanner.py
+++ globalPlanner.py
@@ -1,12 +1,16 @@
-import astar
 from astar import aStar
 
 class GlobalPlanner:
+    schedule = {}
     def __init__(self, grid, agents):
-        #print (grid)
+        print (agents)
         if len(agents) == 1:
             #start = grid[agents[0].x][agents[0].y]
             #goal = grid[agents[0].goal[0]][agents[0].goal[1]]
-            self.schedule = aStar(grid, agents[0].pos, agents[0].goal)
-    
-    
+            self.schedule[agents[0].name] = aStar(grid, agents[0].pos, agents[0].goal)
+        else:
+            for agent in agents:
+                #self.schedule[agent.name] = list(reversed(aStar(grid, agent.pos, agent.goal)))
+                self.schedule[agent.name] = aStar(grid, agent.pos, agent.goal)[::-1]
+
+        print (self.schedule)

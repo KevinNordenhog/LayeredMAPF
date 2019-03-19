a859b83830abedd4b66f1a8a7092fc5b1b2f8efa
KevinNordenhog <kevin.nordenhog@gmail.com>
2019-03-19 12:14:19 +0100

    fixed constraints

--- globalPlanner.py
+++ globalPlanner.py
@@ -2,7 +2,7 @@ from astar import aStar
 
 class GlobalPlanner:
     schedule = {}
-    constraints = {(2,3): [5], (2,5): [3], (3,6): [3], (3,7): [4]}
+    constraints = {}#{(2,3): [5], (2,5): [3], (3,6): [3], (3,7): [4]}
     def __init__(self, grid, agents):
         if len(agents) == 1:
             self.schedule[agents[0].name] = aStar(grid, agents[0].pos, agents[0].goal, self.constraints)#[::-1]

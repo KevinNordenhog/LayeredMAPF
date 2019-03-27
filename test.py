aa43b1fbf0b352df941115fa6f11b7371070c6fc
Joakim <joagyl@student.chalmers.se>
2019-03-27 13:35:12 +0100

    Dynamic environment can be turned on in test

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     size = 10
     density = 20
     agents = 5
-    dynamic_density = 0
-    dynamic_probability = 0
+    dynamic_density = 10
+    dynamic_probability = 10
     world = yaml.load(generatemap.gen_map(size, density, agents,
             dynamic_density, dynamic_probability))
     sim = simulator.Simulator(world)
+    sim.dynamic = True
     sim.simulate()

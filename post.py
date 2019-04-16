a0eb10e3cde30500ec1857d2afb7c8984d67e970
KevinNordenhog <kevin.nordenhog@gmail.com>
2019-04-16 13:55:31 +0200

    Implemented mapf-post to find min window size

---
+++ post.py
@@ -0,0 +1,22 @@
+def post(schedule):
+    windows = []
+    count = {}
+    #loops from 0 to the length of the longest paths
+    for i in range(0, len(schedule[max(schedule, key = lambda x: len(schedule[x]))])):
+        stops = []
+        for agent in schedule:
+            if len(schedule[agent]) > i:
+                if schedule[agent][i] in count:
+                    if not count[schedule[agent][i]][0] == agent:
+                        windows.append(count[schedule[agent][i]][1])
+                        count[schedule[agent][i]] = [agent, 0]
+                        stops.append(schedule[agent][i])
+                else:
+                    count[schedule[agent][i]] = [agent, 0]
+
+        for pos in count:
+            if not pos in stops:
+                count[pos][1] += 1
+
+    print (min(windows))
+    return min(windows)

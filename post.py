0650592964fd9ef0af4abbf2ae3a830d8138f97a
KevinNordenhog <kevin.nordenhog@gmail.com>
2019-05-16 14:53:09 +0200

    Fixed bug in POST

--- post.py
+++ post.py
@@ -3,24 +3,23 @@ import sys
 def post(schedule):
     windows = []
     count = {}
-    #loops from 0 to the length of the longest paths
     if not schedule:
         return -1
+    #loops from 0 to the length of the longest paths
     for i in range(0, len(schedule[max(schedule, key = lambda x: len(schedule[x]))])):
-        stops = []
         for agent in schedule:
             if len(schedule[agent]) > i:
                 if schedule[agent][i] in count:
                     if not count[schedule[agent][i]][0] == agent:
                         windows.append(count[schedule[agent][i]][1])
-                        count[schedule[agent][i]] = [agent, 0]
-                        stops.append(schedule[agent][i])
+                        count[schedule[agent][i]] = [agent, -1]
+                    else:
+                        count[schedule[agent][i]] = [agent,-1]
                 else:
-                    count[schedule[agent][i]] = [agent, 0]
+                    count[schedule[agent][i]] = [agent, -1]
 
         for pos in count:
-            if not pos in stops:
-                count[pos][1] += 1
+            count[pos][1] += 1
     if windows:
         return min(windows)
     else:
